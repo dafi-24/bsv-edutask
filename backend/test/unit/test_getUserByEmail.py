6f8a3de15e38eaf7cc10c14898da54d4bb45c22d
Vinepine <vincentcalle@hotmail.com>
2026-04-08 14:53:54 +0200

    kmom01 done

---
+++ backend/test/unit/test_getUserByEmail.py
@@ -0,0 +1,82 @@
+import pytest
+
+import unittest.mock as mock
+
+from src.controllers.usercontroller import UserController
+
+@pytest.fixture
+def obj():
+    return [{"user":"calle"}, {"user":"anna"}]
+
+@pytest.mark.unit
+def test_checkmail_true(obj):
+
+    mockedController = mock.MagicMock()
+    mockedController.find.return_value = [obj[0]]
+
+    controller = UserController(mockedController)
+
+    assert controller.get_user_by_email("calle@hotmail.com") == obj[0]
+
+@pytest.mark.unit
+def test_checkmail_None():
+
+    mockedController = mock.MagicMock()
+    mockedController.find.return_value = [None]
+
+    controller = UserController(mockedController)
+
+    assert controller.get_user_by_email("calle@hotmail.com") == None
+
+@pytest.mark.unit
+def test_check_multiple_mail_true(obj):
+
+    mockedController = mock.MagicMock()
+    mockedController.find.return_value = [obj[0], obj[1]]
+
+    controller = UserController(mockedController)
+
+    assert controller.get_user_by_email("calle@hotmail.com") == obj[0]
+
+@pytest.mark.unit
+def test_check_multiple_mail_false(obj):
+
+    mockedController = mock.MagicMock()
+    mockedController.find.return_value = [obj[0], obj[1]]
+
+    controller = UserController(mockedController)
+
+    assert controller.get_user_by_email("calle@hotmail.com") != obj[1]
+
+@pytest.mark.unit
+def test_invalid_mail(obj):
+
+    mockedController = mock.MagicMock()
+    mockedController.find.return_value = [obj[0], obj[1]]
+
+    controller = UserController(mockedController)
+
+    with pytest.raises(ValueError):
+        controller.get_user_by_email("callehotmail.com")
+
+@pytest.mark.unit
+def test_no_mail(obj):
+
+    mockedController = mock.MagicMock()
+    mockedController.find.return_value = [obj[0], obj[1]]
+
+    controller = UserController(mockedController)
+
+    with pytest.raises(ValueError):
+        controller.get_user_by_email("")
+
+@pytest.mark.unit
+def test_exception():
+
+    mockedController = mock.MagicMock()
+    mockedController.find.return_value = 1
+
+    controller = UserController(mockedController)
+
+    with pytest.raises(Exception):
+        controller.get_user_by_email("calle@hotmail.com")
